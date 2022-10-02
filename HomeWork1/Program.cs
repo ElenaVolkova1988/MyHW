a9156bfbc1b1cbf0d791b149cce826175ba7fbfd
Elena <xoroshj_chelovek@mail.ru>
2022-10-02 12:45:06 +0300

    Выполнена задача номер 6

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -47,3 +47,17 @@ int b = Convert.ToInt32 (Console.ReadLine ());
                 {Console.WriteLine ( max =c);}
             }
             */
+
+            // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).4 -> да,-3 -> нет,7 -> нет
+
+            Console.WriteLine ("Input number :");
+            int num = Convert.ToInt32 (Console.ReadLine ());
+
+                if (num/2>0)
+                {
+                    Console.WriteLine ("Yes");
+                }
+                else 
+                {
+                    Console.WriteLine ("No");
+                }
