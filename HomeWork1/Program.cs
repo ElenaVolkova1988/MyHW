70a1f031a9afd926ee8b6dac248e75954840327e
Elena <xoroshj_chelovek@mail.ru>
2022-10-02 12:25:18 +0300

    Выполнена задача номер 4

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,5 +1,5 @@
 ﻿/*
-// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.a = 5; b = 7 -> max = 7 a = 2 b = 10 -> max = 10a = -9 b = -3 -> max = -3
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.a = 5; b = 7 -> max = 7, a = 2 b = 10 -> max = 10, a = -9 b = -3 -> max = -3
 
 Console.WriteLine("Input first number : ");
 int a = Convert.ToInt32 (Console.ReadLine ());
@@ -18,3 +18,32 @@ int b = Convert.ToInt32 (Console.ReadLine ());
         Console.WriteLine (max = b);
     }
     */
+
+/*
+    //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. 2, 3, 7 -> 7, 44 5 78 -> 78, 22 3 9 -> 22
+
+    Console.WriteLine ("Input first number :");
+    int a = Convert.ToInt32 (Console.ReadLine ());
+    Console.WriteLine ("Input second number : ");
+    int b = Convert.ToInt32 (Console.ReadLine ());
+    Console.WriteLine ("Input third number : ");
+    int c = Convert.ToInt32 (Console.ReadLine ());
+
+        int max =0; 
+            if (a>b)
+        {
+            max= a; 
+                if (a>c) 
+                {Console.WriteLine (max =a);}
+                else 
+                {Console.WriteLine (max = c);}
+        }
+            else
+        {
+            max = b;
+                if (b>c)
+                {Console.WriteLine (max= b);}
+                else
+                {Console.WriteLine ( max =c);}
+            }
+            */
