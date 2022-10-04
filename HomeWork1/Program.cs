3fc7ace84d87a0bb4018a4111343870180b9d332
Elena <xoroshj_chelovek@mail.ru>
2022-10-04 09:40:06 +0300

    добавила описание словестное

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,4 +1,4 @@
-﻿/*
+﻿
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.a = 5; b = 7 -> max = 7, a = 2 b = 10 -> max = 10, a = -9 b = -3 -> max = -3
 
 Console.WriteLine("Input first number : ");
@@ -10,14 +10,17 @@ int b = Convert.ToInt32 (Console.ReadLine ());
 
     if (a>b)
     {
-        Console.WriteLine ( max = a);
+        Console.Write ("Большее число " );
+
+        Console.WriteLine (max=a);
     }
 
     else 
     {
+        Console.Write ("Большее число " );
         Console.WriteLine (max = b);
     }
-    */
+    
 
 /*
     //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. 2, 3, 7 -> 7, 44 5 78 -> 78, 22 3 9 -> 22
@@ -63,7 +66,7 @@ int b = Convert.ToInt32 (Console.ReadLine ());
                 } 
                 */
 
-
+/*
                 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N. 5 -> 2, 4, 8 -> 2, 4, 6, 8
 
                 Console.WriteLine ("Input number");
@@ -78,5 +81,5 @@ int b = Convert.ToInt32 (Console.ReadLine ());
                     }
                     i++;
                 }
-                    
+                    */
                     
